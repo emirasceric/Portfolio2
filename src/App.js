af88380ad8a49f733c9ea62387bb57b0ce33019f
Emir Asceric <asceric_emir@hotmail.com>
2023-12-11 15:34:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import LoadingPage from './components/LoadingPage';
+import Navbar from './components/NavBar';
+import FirstPage from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // tajmer da se nakon 5 sekundi uvodna stranica skloni
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
+
+        // Očistit tajmer kad se komponenta unmount-a
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+        return <LoadingPage />;
+    }
+
+    return (
+        <div>
+            
+           < Navbar/>
+
+            <section className="section">
+                <FirstPage /> 
+            </section>
+
+            <div className="section">
+                <About />
+            </div>
+            <div className="section">
+                <Contact/>
+            </div>        
+
+           
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.css';
+
+
+function FirstPage() {
+  return (
+    <div className="App">
+      <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css' />
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="stars4"></div>
+
+      <div id="horizon"></div>
+      <div className="glow"></div>
+      <div id="earth"></div>
+      
+
+      <div id="title">Front-End Developer</div>
+      <div id="subtitle">
+        <span>DONT</span>
+        <span>LET</span>
+        <span>GO</span>
+      </div>
+    </div>
+  );
+}
+
+export default FirstPage;
